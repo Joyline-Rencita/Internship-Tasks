aeefcc16a4cda64e9d1b0a7ffacc09190b778f2f
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-05 10:58:53 +0530

    Update 01_KPI's.sql

--- 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
+++ 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
@@ -1,4 +1,4 @@
-#Invoices :
+Invoices :
 COUNT("o_celonis_VendorAccountCreditItem"."ID")
 
 #Average Aging of Invoices :
@@ -12,11 +12,31 @@ AVG(
   END
 )
 
-#avg_past_due_days_v :
+avg_past_due_days_v :
 AVG (KPI("past_due_date"))
 
-#count_table_currencyconversion
+count_table_currencyconversion :
 Count_Table("o_celonis_CurrencyConversion")
 
-#count_exceptiontypesheet1
+count_exceptiontypesheet1 :
 COUNT_TABLE("o_custom_ExceptionTypeSheet1")
+
+count_table_invoice_value_opportunities_sheet1 :
+Count_Table("o_custom_InvoiceValueOpportunitiesSheet1")
+
+count_table_invoicelifecyclestagesheet1 :
+Count_Table("o_custom_InvoiceLifecycleStageSheet1")
+
+count_table_vendorinvoiceitem :
+Count_Table("o_celonis_VendorInvoiceItem")
+
+due_in_future :
+CASE WHEN ADD_DAYS ( "o_celonis_VendorAccountCreditItem"."BaseLineDate",
+KPI("maximun_available_payment_days")) > TODAY() --future
+THEN DAYS_BETWEEN (TODAY(), ADD_DAYS ( "o_celonis_VendorAccountCreditItem"."BaseLineDate",
+KPI("maximun_available_payment_days"))) 
+END
+
+filtered_count :
+COUNT(CASE WHEN {p1} THEN 0 ELSE NULL END)
+
