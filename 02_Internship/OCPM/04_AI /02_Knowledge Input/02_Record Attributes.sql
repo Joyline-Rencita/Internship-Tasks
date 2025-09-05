4b139c737a9140d2aecc8690d20ab7b00b139f3b
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-05 10:44:00 +0530

    Update 02_Record Attributes.sql

--- 02_Internship/OCPM/04_AI /02_Knowledge Input/02_Record Attributes.sql
+++ 02_Internship/OCPM/04_AI /02_Knowledge Input/02_Record Attributes.sql
@@ -3,3 +3,5 @@ OpenInvoiceDeepdiveManualFile    id : O_CUSTOM_OPENINVOICEDEEPDIVEMANUALFILE    
 Companycode :  Company Code is a unique identifier used to represent an independent legal accounting entity within an organization. It defines the smallest unit for which financial statements such as balance sheets and profit & loss statements can be created.
 pql query :  "o_custom_OpenInvoiceDeepdiveManualFile"."COMPANYCODE"
 
+Exception Type : 
+pql query  :  "o_custom_OpenInvoiceDeepdiveManualFile"."ExceptionType"
