bb0b119334f2b465fd9129eaa6d1ae7e17f3cf57
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-05 10:15:59 +0530

    Update 08_Material.sql

--- 02_Internship/OCPM/01_Objects/08_Material.sql
+++ 02_Internship/OCPM/01_Objects/08_Material.sql
@@ -47,3 +47,42 @@ FROM "MARA" AS "MARA"
 --                      ********************************                   CDPOS                    ***************************************
 
 
+SELECT <%=sourceSystem%>  || 'Material_' || "CDPOS"."TABKEY" AS "ObjectID",
+	<%=sourceSystem%>  || "CDPOS"."TABKEY" || "CDPOS"."TABNAME" || "CDPOS"."FNAME"
+       || "CDPOS"."CHANGENR" || "CDPOS"."CHNGIND"            AS "ID",
+       CAST("CDHDR"."UDATE" AS DATE)
+            + CAST(TIMESTAMPDIFF(SECOND, CAST("CDHDR"."UTIME" AS DATE),
+            "CDHDR"."UTIME") AS INTERVAL SECOND)             AS "Time",
+       CASE
+           WHEN "CDPOS"."FNAME" = 'MATKL' THEN 'MaterialGroup'
+           WHEN "CDPOS"."FNAME" = 'MBRSH' THEN 'IndustrySector'
+           WHEN "CDPOS"."FNAME" = 'MTART' THEN 'MaterialType'
+           END                                               AS "Attribute",
+       CASE
+           WHEN "CDPOS"."VALUE_OLD" LIKE '%-' THEN CONCAT('-', REPLACE(LTRIM("CDPOS"."VALUE_OLD"), '-', ''))
+           ELSE "CDPOS"."VALUE_OLD" END                      AS "OldValue",
+       CASE
+           WHEN "CDPOS"."VALUE_NEW" LIKE '%-' THEN CONCAT('-', REPLACE(LTRIM("CDPOS"."VALUE_NEW"), '-', ''))
+           ELSE "CDPOS"."VALUE_NEW" END                      AS "NewValue",
+       'User_' || "CDHDR"."MANDANT" || "CDHDR"."USERNAME"    AS "ChangedBy",
+       "CDHDR"."TCODE"                                       AS "OperationType",
+       "CDHDR"."CHANGENR"                                    AS "OperationID",
+       CASE
+           WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
+           ELSE 'Manual' END                                 AS "ExecutionType"
+FROM "CDPOS" AS "CDPOS"
+         LEFT JOIN "CDHDR" AS "CDHDR" ON
+            "CDPOS"."MANDANT" = "CDHDR"."MANDANT"
+            AND "CDPOS"."OBJECTCLAS" = "CDHDR"."OBJECTCLAS"
+            AND "CDPOS"."OBJECTID" = "CDHDR"."OBJECTID"
+            AND "CDPOS"."CHANGENR" = "CDHDR"."CHANGENR"
+    AND "CDPOS"."OBJECTCLAS" = 'MATERIAL'
+         LEFT JOIN "USR02" AS "USR02"
+                   ON "CDHDR"."USERNAME" = "USR02"."BNAME"
+                       AND "CDHDR"."MANDANT" = "USR02"."MANDT"
+WHERE "CDPOS"."CHNGIND" = 'U'
+  AND "CDPOS"."TABNAME" = 'MARA'
+  AND "CDPOS"."FNAME" IN
+      ('MATKL', 'MBRSH', 'MTART')
+  AND "CDPOS"."MANDANT" IS NOT NULL
+  AND "CDHDR"."MANDANT" IS NOT NULL
