0f373c059a5788fbb038d38d3419e5f2e8ee4148
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-08 09:12:51 +0530

    Update 17_CreatePurchaseRequisitionItem.sql

--- 02_Internship/OCPM/02_Events/17_CreatePurchaseRequisitionItem.sql
+++ 02_Internship/OCPM/02_Events/17_CreatePurchaseRequisitionItem.sql
@@ -8,3 +8,17 @@ FROM "o_celonis_PurchaseRequisitionItem" AS "PurchaseRequisition"
          LEFT JOIN "o_celonis_ContractItem" AS "ContractItem"
                    ON "PurchaseRequisition"."ContractItem_ID" = "ContractItem"."ID"
 WHERE "PurchaseRequisition"."CreationTime" IS NOT NULL
+
+
+==================================================================================================================================
+
+SELECT DISTINCT "Event"."ID"  AS "ID",
+                "Object"."ID" AS "PurchaseOrderItems"
+FROM "e_celonis_CreatePurchaseRequisitionItem" AS "Event"
+     LEFT JOIN "o_celonis_PurchaseRequisitionItem" AS "PurchaseRequisitionItem"
+               ON "Event"."PurchaseRequisitionItem_ID" = "PurchaseRequisitionItem"."ID"
+     LEFT JOIN "r_o_celonis_PurchaseRequisitionItem__PurchaseOrderItems" AS "r_PurchaseOrderItems"
+               ON "PurchaseRequisitionItem"."ID" = "r_PurchaseOrderItems"."ID"
+     LEFT JOIN "o_celonis_PurchaseOrderItem" AS "Object"
+               ON "r_PurchaseOrderItems"."PurchaseOrderItems_ID" = "Object"."ID"
+WHERE "Object"."ID" IS NOT NULL
