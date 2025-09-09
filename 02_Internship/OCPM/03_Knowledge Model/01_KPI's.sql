fd8d6830ef738b87368ea68a8e872d7086680006
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-09 19:13:03 +0530

    Update 01_KPI's.sql

--- 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
+++ 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
@@ -180,4 +180,15 @@ Total Purchase OrderItems :
 COUNT_TABLE("o_celonis_PurchaseOrderItem")
 
 
+v_pl_req_qty :
+"o_celonis_PurchaseOrderItem"."Quantity" * ((100 - TO_INT("o_celonis_PurchaseOrderItem"."ToleranceLimit"))/100)
+
+
+
+
+
+
+
+
+
 
