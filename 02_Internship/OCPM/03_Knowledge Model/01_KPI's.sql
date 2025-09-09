3f934651f0a9353d420cb34799dd527e650936df
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-09 19:11:11 +0530

    Update 01_KPI's.sql

--- 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
+++ 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
@@ -140,6 +140,14 @@ THEN CURRENCY_CONVERT(
 )
 
 
+Open Invoice Lines By Volume :
+COUNT(
+   CASE WHEN "o_celonis_VendorAccountCreditItem"."ClearingDate" IS NULL
+        THEN "o_custom_OpenInvoiceDeepdiveManualFile"."ID"
+   END
+)
+
+
 
 
 
