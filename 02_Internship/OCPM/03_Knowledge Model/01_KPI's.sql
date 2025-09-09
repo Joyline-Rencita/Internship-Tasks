8995a360afa3889a8ad6a5cc42db5fd7cfc1ac73
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-09 19:09:49 +0530

    Update 01_KPI's.sql

--- 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
+++ 02_Internship/OCPM/03_Knowledge Model/01_KPI's.sql
@@ -100,6 +100,17 @@ COUNT(DISTINCT CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."Status" = 'In
 then "o_custom_OpenInvoiceDeepdiveManualFile"."BELNR"||"o_custom_OpenInvoiceDeepdiveManualFile"."BUKRS"||"o_custom_OpenInvoiceDeepdiveManualFile"."GJAHR" END)
 
 
+Maximun Available Payment days :
+-- ADD_DAYS("o_celonis_VendorAccountCreditItem"."BaseLineDate",CASE WHEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays3" > 0 THEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays3" WHEN
+--   "o_celonis_VendorAccountCreditItem"."VendorPaymentDays2" > 0 THEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays2" WHEN
+--   "o_celonis_VendorAccountCreditItem"."VendorPaymentDays1" > 0 THEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays1" ELSE 0 END) 
+
+ROUND(CASE WHEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays3" > 0 THEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays3" WHEN
+  "o_celonis_VendorAccountCreditItem"."VendorPaymentDays2" > 0 AND "o_celonis_VendorAccountCreditItem"."VendorPaymentDays3" = 0 THEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays2" WHEN
+  "o_celonis_VendorAccountCreditItem"."VendorPaymentDays1" > 0 AND "o_celonis_VendorAccountCreditItem"."VendorPaymentDays3" = 0 AND "o_celonis_VendorAccountCreditItem"."VendorPaymentDays2" = 0
+  THEN "o_celonis_VendorAccountCreditItem"."VendorPaymentDays1" ELSE 0 END)
+
+
 
 
 
