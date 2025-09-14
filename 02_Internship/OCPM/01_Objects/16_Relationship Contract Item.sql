0f0b570a27cd8ca77306468acc3645d2ec239220
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-14 11:06:03 +0530

    Update 16_Relationship Contract Item.sql

--- 02_Internship/OCPM/01_Objects/16_Relationship Contract Item.sql
+++ 02_Internship/OCPM/01_Objects/16_Relationship Contract Item.sql
@@ -2,49 +2,49 @@
 
 
 WITH "CTE_RCI" AS (
-        SELECT "ContractItem"."ID"                      AS "ContractItem_ID",
-               "PurchaseOrderItem"."ID"                 AS "PurchaseOrderItem_ID",
-               "PurchaseRequisitionItem_JoinTable"."ID" AS "PurchaseRequisitionItem_ID"
-        FROM "o_celonis_PurchaseOrderItem" AS "PurchaseOrderItem"
-                 INNER JOIN "r_o_celonis_PurchaseRequisitionItem__PurchaseOrderItems" AS "PurchaseRequisitionItem_JoinTable"
+        SELECT "ContractItem"."ID"                                                      AS "ContractItem_ID",
+               "PurchaseOrderItem"."ID"                                                 AS "PurchaseOrderItem_ID",
+               "PurchaseRequisitionItem_JoinTable"."ID"                                 AS "PurchaseRequisitionItem_ID"
+        FROM "o_celonis_PurchaseOrderItem"                                              AS "PurchaseOrderItem"
+                 INNER JOIN "r_o_celonis_PurchaseRequisitionItem__PurchaseOrderItems"   AS "PurchaseRequisitionItem_JoinTable"
                             ON "PurchaseOrderItem"."ID"
                                = "PurchaseRequisitionItem_JoinTable"."PurchaseOrderItems_ID"
-                 INNER JOIN "o_celonis_ContractItem" AS "ContractItem"
+                 INNER JOIN "o_celonis_ContractItem"                                     AS "ContractItem"
                             ON "PurchaseOrderItem"."ContractItem_ID" = "ContractItem"."ID"
         UNION
-        SELECT "ContractItem"."ID"                                         AS "ContractItem_ID",
-               "PurchaseRequisitionItem_JoinTable"."PurchaseOrderItems_ID" AS "PurchaseOrderItem_ID",
-               "PurchaseRequisitionItem_JoinTable"."ID"                    AS "PurchaseRequisitionItem_ID"
-        FROM "o_celonis_PurchaseRequisitionItem" AS "PurchaseRequisitionItem"
-                 INNER JOIN "r_o_celonis_PurchaseRequisitionItem__PurchaseOrderItems" AS "PurchaseRequisitionItem_JoinTable"
+        SELECT "ContractItem"."ID"                                                       AS "ContractItem_ID",
+               "PurchaseRequisitionItem_JoinTable"."PurchaseOrderItems_ID"               AS "PurchaseOrderItem_ID",
+               "PurchaseRequisitionItem_JoinTable"."ID"                                  AS "PurchaseRequisitionItem_ID"
+        FROM "o_celonis_PurchaseRequisitionItem"                                         AS "PurchaseRequisitionItem"
+                 INNER JOIN "r_o_celonis_PurchaseRequisitionItem__PurchaseOrderItems"    AS "PurchaseRequisitionItem_JoinTable"
                             ON "PurchaseRequisitionItem"."ID" = "PurchaseRequisitionItem_JoinTable"."ID"
-                 INNER JOIN "o_celonis_ContractItem" AS "ContractItem"
+                 INNER JOIN "o_celonis_ContractItem"                                     AS "ContractItem"
                             ON "PurchaseRequisitionItem"."ContractItem_ID" = "ContractItem"."ID"),
-   "CTE_RCITE_1" AS (SELECT NULL                                       AS "ContractItem_ID",
-                              "PurchaseOrderItem"."ID"                 AS "PurchaseOrderItem_ID",
-                              "PurchaseRequisitionItem_JoinTable"."ID" AS "PurchaseRequisitionItem_ID"
-                       FROM "o_celonis_PurchaseOrderItem" AS "PurchaseOrderItem"
+   "CTE_RCITE_1" AS (SELECT NULL                                                         AS "ContractItem_ID",
+                              "PurchaseOrderItem"."ID"                                   AS "PurchaseOrderItem_ID",
+                              "PurchaseRequisitionItem_JoinTable"."ID"                   AS "PurchaseRequisitionItem_ID"
+                       FROM "o_celonis_PurchaseOrderItem"                                AS "PurchaseOrderItem"
                                 INNER JOIN "r_o_celonis_PurchaseRequisitionItem__PurchaseOrderItems" AS "PurchaseRequisitionItem_JoinTable"
                                            ON "PurchaseOrderItem"."ID"
                                               = "PurchaseRequisitionItem_JoinTable"."PurchaseOrderItems_ID"
                        EXCEPT
-                       SELECT NULL                         AS "ContractItem_ID",
-                              "PurchaseOrderItem_ID"       AS "PurchaseOrderItem_ID",
-                              "PurchaseRequisitionItem_ID" AS "PurchaseRequisitionItem_ID"
+                       SELECT NULL                                                        AS "ContractItem_ID",
+                              "PurchaseOrderItem_ID"                                      AS "PurchaseOrderItem_ID",
+                              "PurchaseRequisitionItem_ID"                                AS "PurchaseRequisitionItem_ID"
                        FROM "CTE_RCI"),
-   "CTE_RCITE_2" AS (SELECT "ContractItem"."ID"        AS "ContractItem_ID",
-                              "PurchaseOrderItem"."ID" AS "PurchaseOrderItem_ID",
-                              NULL                     AS "PurchaseRequisitionItem_ID"
-                       FROM "o_celonis_PurchaseOrderItem" AS "PurchaseOrderItem"
-                                INNER JOIN "o_celonis_ContractItem" AS "ContractItem"
+   "CTE_RCITE_2" AS (SELECT "ContractItem"."ID"                                           AS "ContractItem_ID",
+                              "PurchaseOrderItem"."ID"                                    AS "PurchaseOrderItem_ID",
+                              NULL                                                        AS "PurchaseRequisitionItem_ID"
+                       FROM "o_celonis_PurchaseOrderItem"                                 AS "PurchaseOrderItem"
+                                INNER JOIN "o_celonis_ContractItem"                       AS "ContractItem"
                                            ON "PurchaseOrderItem"."ContractItem_ID" = "ContractItem"."ID"
                        EXCEPT
-                       SELECT "ContractItem_ID"      AS "ContractItem_ID",
-                              "PurchaseOrderItem_ID" AS "PurchaseOrderItem_ID",
-                              NULL                   AS "PurchaseRequisitionItem_ID"
+                       SELECT "ContractItem_ID"                                            AS "ContractItem_ID",
+                              "PurchaseOrderItem_ID"                                       AS "PurchaseOrderItem_ID",
+                              NULL                                                         AS "PurchaseRequisitionItem_ID"
                        FROM "CTE_RCI"),
-   "CTE_RCITE_3" AS (SELECT "ContractItem"."ID"              AS "ContractItem_ID",
-                              NULL                           AS "PurchaseOrderItem_ID",
+   "CTE_RCITE_3" AS (SELECT "ContractItem"."ID"                                            AS "ContractItem_ID",
+                              NULL                                                         AS "PurchaseOrderItem_ID",
                               "PurchaseRequisitionItem"."ID" AS "PurchaseRequisitionItem_ID"
                        FROM "o_celonis_PurchaseRequisitionItem" AS "PurchaseRequisitionItem"
                                 INNER JOIN "o_celonis_ContractItem" AS "ContractItem"
