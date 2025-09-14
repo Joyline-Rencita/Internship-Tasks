5dcc0729462b9b46e0e1347de3a35e930ed45e41
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-14 10:35:38 +0530

    Update 57_ SalesOrder.sql

--- 02_Internship/OCPM/01_Objects/57_ SalesOrder.sql
+++ 02_Internship/OCPM/01_Objects/57_ SalesOrder.sql
@@ -96,3 +96,112 @@ WHERE "VBAK"."MANDT" IS NOT NULL
                     *************************        CDPOS      *****************************
 
 
+SELECT <%=sourceSystem%>  || 'SalesOrder_' || "CDPOS"."TABKEY" AS "ObjectID",
+	<%=sourceSystem%>  || "CDPOS"."TABKEY" || "CDPOS"."TABNAME" || "CDPOS"."FNAME"
+       || "CDPOS"."CHANGENR" || "CDPOS"."CHNGIND"              AS "ID",
+       CAST("CDHDR"."UDATE" AS DATE)
+            + CAST(TIMESTAMPDIFF(SECOND, CAST("CDHDR"."UTIME" AS DATE),
+            "CDHDR"."UTIME") AS INTERVAL SECOND)               AS "Time",
+       CASE
+           WHEN "CDPOS"."FNAME" = 'VDATU' THEN 'RequestedDeliveryDate'
+           END                                                 AS "Attribute",
+       CASE
+           WHEN "CDPOS"."VALUE_OLD" LIKE '%-' THEN CONCAT('-', REPLACE(LTRIM("CDPOS"."VALUE_OLD"), '-', ''))
+           ELSE "CDPOS"."VALUE_OLD" END                        AS "OldValue",
+       CASE
+           WHEN "CDPOS"."VALUE_NEW" LIKE '%-' THEN CONCAT('-', REPLACE(LTRIM("CDPOS"."VALUE_NEW"), '-', ''))
+           ELSE "CDPOS"."VALUE_NEW" END                        AS "NewValue",
+       'User_' || "CDHDR"."MANDANT" || "CDHDR"."USERNAME"      AS "ChangedBy",
+       "CDHDR"."TCODE"                                         AS "OperationType",
+       "CDHDR"."CHANGENR"                                      AS "OperationID",
+       CASE
+           WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
+           ELSE 'Manual' END                                   AS "ExecutionType"
+FROM "CDPOS" AS "CDPOS"
+         LEFT JOIN "CDHDR" AS "CDHDR" ON
+    "CDPOS"."MANDANT" = "CDHDR"."MANDANT"
+    AND "CDPOS"."OBJECTCLAS" = "CDHDR"."OBJECTCLAS"
+    AND "CDPOS"."OBJECTID" = "CDHDR"."OBJECTID"
+    AND "CDPOS"."CHANGENR" = "CDHDR"."CHANGENR"
+        AND "CDPOS"."OBJECTCLAS" = 'VERKBELEG'
+         LEFT JOIN "USR02" AS "USR02"
+                   ON "CDHDR"."USERNAME" = "USR02"."BNAME"
+                       AND "CDHDR"."MANDANT" = "USR02"."MANDT"
+WHERE "CDPOS"."CHNGIND" = 'U'
+  AND "CDPOS"."TABNAME" = 'VBAK'
+  AND "CDPOS"."FNAME" IN ('VDATU')
+  AND "CDPOS"."MANDANT" IS NOT NULL
+  AND "CDHDR"."MANDANT" IS NOT NULL
+
+=================================================================================================================================================================
+
+
+							***************************		JCDS		*******************************
+
+
+SELECT <%=sourceSystem%>  || 'SalesOrder_' || "VBAK"."MANDT" || "VBAK"."VBELN"                     AS "ObjectID",
+	<%=sourceSystem%>  || "VBAK"."VBELN" || "JCDS"."MANDT" || "JCDS"."OBJNR"
+       || "JCDS"."STAT" || "JCDS"."CHGNR"                                                          AS "ID",
+       CAST("JCDS"."UDATE" AS DATE)
+            + CAST(TIMESTAMPDIFF(SECOND, CAST("JCDS"."UTIME" AS DATE),
+            "JCDS"."UTIME") AS INTERVAL SECOND)                                                    AS "Time",
+       'DocumentStatus'                                                                            AS "Attribute",
+       LAG("JCDS"."STAT", 1, NULL)
+       OVER (PARTITION BY "JCDS"."OBJNR", "JCDS"."STAT"
+           ORDER BY "JCDS"."OBJNR", "JCDS"."STAT", "JCDS"."CHGNR", "JCDS"."UDATE", "JCDS"."UTIME") AS "OldValue",
+       "JCDS"."STAT"                                                                               AS "NewValue",
+       'User_' || "JCDS"."USNAM"                                                                   AS "ChangedBy",
+       "JCDS"."CDTCODE"                                                                            AS "OperationType",
+       CAST("JCDS"."CHGNR" AS VARCHAR)                                                             AS "OperationID",
+       CASE
+           WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
+           ELSE 'Manual' END                                                                       AS "ExecutionType"
+FROM "JCDS" AS "JCDS"
+         LEFT JOIN "VBAK" AS "VBAK"
+                   ON "JCDS"."OBJNR" = "VBAK"."OBJNR"
+         LEFT JOIN "USR02" AS "USR02"
+                   ON "JCDS"."USNAM" = "USR02"."BNAME"
+                       AND "JCDS"."MANDT" = "USR02"."MANDT"
+WHERE "VBAK"."VBTYP" IN ('C', 'I')
+  AND "JCDS"."INACT" IS NULL
+  AND "JCDS"."UDATE" IS NOT NULL
+  AND "JCDS"."MANDT" IS NOT NULL
+  AND "VBAK"."MANDT" IS NOT NULL
+
+
+===================================================================================================================================================================
+
+
+									******************************		VBAK 1		**********************************
+
+
+SELECT <%=sourceSystem%>  || 'SalesOrder_' || "VBAK"."MANDT" || "VBAK"."VBELN" AS "ObjectID",
+	<%=sourceSystem%>  || "NAST"."MANDT" || "NAST"."KAPPL" || "NAST"."OBJKY"
+       || "NAST"."KSCHL" || "NAST"."SPRAS" || COALESCE("NAST"."PARNR", '')
+       || COALESCE("NAST"."PARVW", '') || "NAST"."ERDAT" || "NAST"."ERUHR"     AS "ID",
+       CAST("NAST"."DATVR" AS DATE)
+            + CAST(TIMESTAMPDIFF(SECOND, CAST("NAST"."UHRVR" AS DATE),
+            "NAST"."UHRVR") AS INTERVAL SECOND)                                AS "Time",
+       CASE
+           WHEN "NAST"."KSCHL" = 'BA00' THEN 'SendOrderConfirmation'
+           END                                                                 AS "Attribute",
+       NULL                                                                    AS "OldValue",
+       NULL                                                                    AS "NewValue",
+       'User_' || "NAST"."MANDT" || "NAST"."USNAM"                             AS "ChangedBy",
+       "NAST"."TCODE"                                                          AS "OperationType",
+       NULL                                                                    AS "OperationID",
+       CASE
+           WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
+           ELSE 'Manual' END                                                   AS "ExecutionType"
+FROM "VBAK" AS "VBAK"
+         LEFT JOIN "NAST" AS "NAST"
+                   ON "VBAK"."VBELN" = "NAST"."OBJKY"
+                       AND "VBAK"."MANDT" = "NAST"."MANDT"
+                       AND "NAST"."DATVR" IS NOT NULL
+                       AND "NAST"."KAPPL" = 'V1'
+                       AND "NAST"."KSCHL" IN ('BA00')
+         LEFT JOIN "USR02" AS "USR02"
+                   ON "NAST"."MANDT" = "USR02"."MANDT"
+                       AND "NAST"."USNAM" = "USR02"."BNAME"
+WHERE "VBAK"."MANDT" IS NOT NULL
+  AND "NAST"."MANDT" IS NOT NULL
