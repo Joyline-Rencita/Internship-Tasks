6cc56b47ee5ca02c118cf60e218e6e69be3c27d9
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-15 20:29:34 +0530

    Update 27_Vendor Invoice.sql

--- 02_Internship/OCPM/01_Objects/27_Vendor Invoice.sql
+++ 02_Internship/OCPM/01_Objects/27_Vendor Invoice.sql
@@ -68,27 +68,27 @@ WHERE "RBKP"."MANDT" IS NOT NULL
                         ****************************      CDPOS  *****************************
 
 
-SELECT <%=sourceSystem%>  || 'VendorInvoice_' || "CDPOS"."TABKEY" 				AS "ObjectID",
+SELECT <%=sourceSystem%>  || 'VendorInvoice_' || "CDPOS"."TABKEY" 						AS "ObjectID",
 	<%=sourceSystem%>  || "CDPOS"."TABKEY" || "CDPOS"."TABNAME" || "CDPOS"."FNAME"
-       || "CDPOS"."CHANGENR" || "CDPOS"."CHNGIND"                 				AS "ID",
+       || "CDPOS"."CHANGENR" || "CDPOS"."CHNGIND"                 						AS "ID",
        CAST("CDHDR"."UDATE" AS DATE) + CAST(TIMESTAMPDIFF(SECOND, CAST("CDHDR"."UTIME" AS DATE),
-            "CDHDR"."UTIME") AS INTERVAL SECOND)                  				AS "Time",
+            "CDHDR"."UTIME") AS INTERVAL SECOND)                  						AS "Time",
        CASE
            WHEN "CDPOS"."FNAME" = 'MWSKZ1' THEN 'TaxCode'
            WHEN "CDPOS"."FNAME" = 'ZTERM' THEN 'PaymentTerms'
-           END                                                    				AS "Attribute",
+           END                                                    						AS "Attribute",
        CASE
            WHEN "CDPOS"."VALUE_OLD" LIKE '%-' THEN CONCAT('-', REPLACE(LTRIM("CDPOS"."VALUE_OLD"), '-', ''))
-           ELSE "CDPOS"."VALUE_OLD" END                           				AS "OldValue",
+           ELSE "CDPOS"."VALUE_OLD" END                           						AS "OldValue",
        CASE
            WHEN "CDPOS"."VALUE_NEW" LIKE '%-' THEN CONCAT('-', REPLACE(LTRIM("CDPOS"."VALUE_NEW"), '-', ''))
-           ELSE "CDPOS"."VALUE_NEW" END                           				AS "NewValue",
-       'User_' || "CDHDR"."MANDANT" || "CDHDR"."USERNAME"         				AS "ChangedBy",
-       "CDHDR"."TCODE"                                            				AS "OperationType",
-       "CDHDR"."CHANGENR"                                         				AS "OperationID",
+           ELSE "CDPOS"."VALUE_NEW" END                           						AS "NewValue",
+       'User_' || "CDHDR"."MANDANT" || "CDHDR"."USERNAME"         						AS "ChangedBy",
+       "CDHDR"."TCODE"                                            						AS "OperationType",
+       "CDHDR"."CHANGENR"                                         						AS "OperationID",
        CASE
            WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
-           ELSE 'Manual' END                                      				AS "ExecutionType"
+           ELSE 'Manual' END                                      						AS "ExecutionType"
 FROM "CDPOS" AS "CDPOS"
          LEFT JOIN "CDHDR" AS "CDHDR"
                    ON "CDPOS"."MANDANT" = "CDHDR"."MANDANT"
