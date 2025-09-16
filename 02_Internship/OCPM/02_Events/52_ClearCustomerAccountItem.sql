587ef2afb5d5038852d4bb6c117aea422f6b11d3
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-16 20:21:07 +0530

    Update 52_ClearCustomerAccountItem.sql

--- 02_Internship/OCPM/02_Events/52_ClearCustomerAccountItem.sql
+++ 02_Internship/OCPM/02_Events/52_ClearCustomerAccountItem.sql
@@ -8,3 +8,18 @@ WHERE "CustomerAccountClearingAssignment"."CreationTime" IS NOT NULL
 
 =======================================================================================================================================================================
 
+                            Relationships =>  CustomerAccountClearingAssignment
+
+SELECT "Event"."ID"  AS "ID",
+       "Object"."ID" AS "CustomerAccountCreditItems"
+FROM "e_celonis_ClearCustomerAccountItem" AS "Event"
+         LEFT JOIN "o_celonis_CustomerAccountClearingAssignment" AS "Clearing"
+                   ON "Event"."CustomerAccountClearingAssignment_ID" = "Clearing"."ID"
+         LEFT JOIN "r_o_celonis_CustomerAccountClearingAssignment__CustomerAccountCreditItems" AS "r_CreditItem"
+                   ON "Clearing"."ID" = "r_CreditItem"."ID"
+         LEFT JOIN "o_celonis_CustomerAccountCreditItem" AS "Object"
+                   ON "r_CreditItem"."CustomerAccountCreditItems_ID" = "Object"."ID"
+WHERE "Object"."ID" IS NOT NULL
+
+=======================================================================================================================================================================
+
