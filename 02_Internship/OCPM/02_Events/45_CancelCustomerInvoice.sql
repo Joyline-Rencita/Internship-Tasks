1b25778777f4eb19247d148bc21b6133b74265be
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-16 20:14:53 +0530

    Update 45_CancelCustomerInvoice.sql

--- 02_Internship/OCPM/02_Events/45_CancelCustomerInvoice.sql
+++ 02_Internship/OCPM/02_Events/45_CancelCustomerInvoice.sql
@@ -26,3 +26,24 @@ WHERE TIMESTAMPDIFF(SECOND, "Event"."Time", "Object"."CreationTime") <= 5
 
                           Relationships ==>  Cancel Customer Invoice 2
 
+WITH "CTE_AUX" AS (SELECT "Event"."ID"  AS "ID",
+                          "Object"."ID" AS "CustomerInvoiceItems_ID",
+                          "Event"."Time",
+                          "Object"."CreationTime"
+                   FROM "e_celonis_CancelCustomerInvoice" AS "Event"
+                            LEFT JOIN "o_celonis_CustomerInvoiceCancellation" AS "CustomerInvoiceCancellation"
+                                      ON "Event"."CustomerInvoiceCancellation_ID" = "CustomerInvoiceCancellation"."ID"
+                            LEFT JOIN "o_celonis_CustomerInvoiceCancellationItem" AS "CustomerInvoiceCancellationItem"
+                                      ON "CustomerInvoiceCancellation"."ID"
+                                         = "CustomerInvoiceCancellationItem"."Header_ID"
+                            LEFT JOIN (SELECT *
+                                       FROM "r_o_celonis_CustomerInvoiceCancellationItem__CustomerInvoiceItems"
+                                       ORDER BY "CustomerInvoiceItems_ID") AS "CustomerInvoiceItem"
+                                      ON "CustomerInvoiceCancellationItem"."ID" = "CustomerInvoiceItem"."ID"
+                            LEFT JOIN (SELECT * FROM "o_celonis_CustomerInvoiceItem") AS "Object"
+                                      ON "CustomerInvoiceItem"."CustomerInvoiceItems_ID" = "Object"."ID")
+SELECT DISTINCT "ID"                      AS "ID",
+                "CustomerInvoiceItems_ID" AS "CustomerInvoiceItems"
+FROM "CTE_AUX" AS "AUX"
+WHERE TIMESTAMPDIFF(SECOND, "Time", "CreationTime") <= 5
+  AND "CustomerInvoiceItems_ID" IS NOT NULL
