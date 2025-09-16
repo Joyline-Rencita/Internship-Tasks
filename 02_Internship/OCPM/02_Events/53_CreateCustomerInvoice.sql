136979fd0e79e7b2bc211359f142f0e60c3f5ef9
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-16 20:30:39 +0530

    Update 53_CreateCustomerInvoice.sql

--- 02_Internship/OCPM/02_Events/53_CreateCustomerInvoice.sql
+++ 02_Internship/OCPM/02_Events/53_CreateCustomerInvoice.sql
@@ -8,3 +8,56 @@ WHERE "CustomerInvoice"."CreationTime" IS NOT NULL
 
 =======================================================================================================================================================================
 
+                                          Relationships =>   Customer Invoice Items
+
+SELECT DISTINCT
+                "Event"."ID"  AS "ID",
+                "Object"."ID" AS "CustomerInvoiceItems"
+FROM "e_celonis_CreateCustomerInvoice" AS "Event"
+         LEFT JOIN (SELECT "Object"."Header_ID"    AS "Header_ID",
+                           "Object"."ID"           AS "ID",
+                           "Object"."CreationTime" AS "CreationTime"
+                    FROM "o_celonis_CustomerInvoiceItem" AS "Object"
+                    ORDER BY "Object"."Header_ID") AS "Object"
+                   ON "Event"."CustomerInvoice_ID" = "Object"."Header_ID"
+WHERE TIMESTAMPDIFF(SECOND, "Event"."Time", "Object"."CreationTime") <= 5
+  AND "Object"."ID" IS NOT NULL
+
+
+======================================================================================================================================================================
+
+                                          Relationships =>   Delivery Items
+
+
+SELECT DISTINCT
+       "Event"."ID"  AS "ID",
+       "Object"."ID" AS "DeliveryItems"
+FROM (SELECT "Event"."ID"               AS "ID",
+             "Event"."Time"             AS "Time",
+             "CustomerInvoiceItem"."ID" AS "CustomerInvoiceItem_Id"
+      FROM "e_celonis_CreateCustomerInvoice" AS "Event"
+               LEFT JOIN (SELECT "Object"."Header_ID"    AS "Header_ID",
+                                 "Object"."ID"           AS "ID",
+                                 "Object"."CreationTime" AS "CreationTime"
+                          FROM "o_celonis_CustomerInvoiceItem" AS "Object"
+                          ORDER BY "Object"."Header_ID") AS "CustomerInvoiceItem"
+                         ON "Event"."CustomerInvoice_ID" = "CustomerInvoiceItem"."Header_ID"
+      ORDER BY "CustomerInvoiceItem"."ID") AS "Event"
+         LEFT JOIN (SELECT "CustomerInvoiceItem_DeliveryItems"."ID" AS "CustomerInvoiceItem_DeliveryItems_ID",
+                           "Object"."CreationTime"                  AS "CreationTime",
+                           "Object"."ID"                            AS "ID"
+                    FROM (SELECT "CustomerInvoiceItem_DeliveryItems"."ID"               AS "ID",
+                                 "CustomerInvoiceItem_DeliveryItems"."DeliveryItems_ID" AS "DeliveryItems_ID"
+                          FROM "r_o_celonis_CustomerInvoiceItem__DeliveryItems" AS "CustomerInvoiceItem_DeliveryItems"
+                          ORDER BY "CustomerInvoiceItem_DeliveryItems"."DeliveryItems_ID") AS "CustomerInvoiceItem_DeliveryItems"
+                             LEFT JOIN "o_celonis_DeliveryItem" AS "Object"
+                                       ON "CustomerInvoiceItem_DeliveryItems"."DeliveryItems_ID" = "Object"."ID"
+                    ORDER BY "CustomerInvoiceItem_DeliveryItems"."ID") AS "Object"
+                   ON "Event"."CustomerInvoiceItem_Id" = "Object"."CustomerInvoiceItem_DeliveryItems_ID"
+WHERE TIMESTAMPDIFF(SECOND, "Event"."Time", "Object"."CreationTime") <= 5
+  AND "Object"."ID" IS NOT NULL
+
+====================================================================================================================================================================
+
+                                          Relationships =>   Customer Invoice Items
+
