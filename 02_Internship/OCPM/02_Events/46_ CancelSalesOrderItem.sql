1c67415aa285ad6a86098aa6ab480b38a91a8bd5
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-16 20:34:44 +0530

    Update 46_ CancelSalesOrderItem.sql

--- 02_Internship/OCPM/02_Events/46_ CancelSalesOrderItem.sql
+++ 02_Internship/OCPM/02_Events/46_ CancelSalesOrderItem.sql
@@ -1,10 +1,11 @@
-SELECT 'CancelSalesOrderItem' || '_' || "SalesOrderItem_Changes"."ID" AS "ID",
-       "SalesOrderItem"."ID"                                          AS "SalesOrderItem",
-       "SalesOrderItem_Changes"."Time"                                AS "Time",
-       "SalesOrderItem_Changes"."ChangedBy"                           AS "ExecutedBy",
-       "SalesOrderItem_Changes"."ExecutionType"                       AS "ExecutionType",
-       "SalesOrderItem_Changes"."OldValue"                            AS "OldValue",
-       "SalesOrderItem_Changes"."NewValue"                            AS "NewValue"
+SELECT 'CancelSalesOrderItem' || '_' || "SalesOrderItem_Changes"."ID"        AS "ID",
+       "SalesOrderItem"."ID"                                                 AS "SalesOrderItem",
+       "SalesOrderItem_Changes"."Time"                                       AS "Time",
+       "SalesOrderItem_Changes"."ChangedBy"                                  AS "ExecutedBy",
+       "SalesOrderItem_Changes"."ExecutionType"                              AS "ExecutionType",
+       "SalesOrderItem_Changes"."OldValue"                                   AS "OldValue",
+       "SalesOrderItem_Changes"."NewValue"                                   AS "NewValue"
+       
 FROM "o_celonis_SalesOrderItem" AS "SalesOrderItem"
          LEFT JOIN "c_o_celonis_SalesOrderItem" AS "SalesOrderItem_Changes"
                    ON "SalesOrderItem"."ID" = "SalesOrderItem_Changes"."ObjectID"
