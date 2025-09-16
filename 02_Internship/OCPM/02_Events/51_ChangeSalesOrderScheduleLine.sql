cb49e98482a738e1b98ef77aacf01e5e19491729
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-16 20:25:07 +0530

    Update 51_ChangeSalesOrderScheduleLine.sql

--- 02_Internship/OCPM/02_Events/51_ChangeSalesOrderScheduleLine.sql
+++ 02_Internship/OCPM/02_Events/51_ChangeSalesOrderScheduleLine.sql
@@ -1,11 +1,12 @@
-SELECT 'ChangeSalesOrderScheduleLine' || '_' || "SalesOrderScheduleLine_Changes"."ID" AS "ID",
-       "SalesOrderScheduleLine"."ID"                                                  AS "SalesOrderScheduleLine",
-       "SalesOrderScheduleLine_Changes"."Time"                                        AS "Time",
-       "SalesOrderScheduleLine_Changes"."ChangedBy"                                   AS "ExecutedBy",
-       "SalesOrderScheduleLine_Changes"."Attribute"                                   AS "ChangedAttribute",
-       "SalesOrderScheduleLine_Changes"."ExecutionType"                               AS "ExecutionType",
-       "SalesOrderScheduleLine_Changes"."OldValue"                                    AS "OldValue",
-       "SalesOrderScheduleLine_Changes"."NewValue"                                    AS "NewValue"
+SELECT 'ChangeSalesOrderScheduleLine' || '_' || "SalesOrderScheduleLine_Changes"."ID"        AS "ID",
+       "SalesOrderScheduleLine"."ID"                                                         AS "SalesOrderScheduleLine",
+       "SalesOrderScheduleLine_Changes"."Time"                                               AS "Time",
+       "SalesOrderScheduleLine_Changes"."ChangedBy"                                          AS "ExecutedBy",
+       "SalesOrderScheduleLine_Changes"."Attribute"                                          AS "ChangedAttribute",
+       "SalesOrderScheduleLine_Changes"."ExecutionType"                                      AS "ExecutionType",
+       "SalesOrderScheduleLine_Changes"."OldValue"                                           AS "OldValue",
+       "SalesOrderScheduleLine_Changes"."NewValue"                                           AS "NewValue"
+       
 FROM "o_celonis_SalesOrderScheduleLine" AS "SalesOrderScheduleLine"
          LEFT JOIN "c_o_celonis_SalesOrderScheduleLine" AS "SalesOrderScheduleLine_Changes"
                    ON "SalesOrderScheduleLine"."ID" = "SalesOrderScheduleLine_Changes"."ObjectID"
