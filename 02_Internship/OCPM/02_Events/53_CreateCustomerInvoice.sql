0d479ea93be41b7fc27458403bc8e09c34101956
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-16 20:31:29 +0530

    Update 53_CreateCustomerInvoice.sql

--- 02_Internship/OCPM/02_Events/53_CreateCustomerInvoice.sql
+++ 02_Internship/OCPM/02_Events/53_CreateCustomerInvoice.sql
@@ -1,9 +1,10 @@
-SELECT 'CreateCustomerInvoice' || '_' || "CustomerInvoice"."ID" AS "ID",
-       "CustomerInvoice"."ID"                                   AS "CustomerInvoice",
-       "CustomerInvoice"."CreationTime"                         AS "Time",
-       "CustomerInvoice"."CreatedBy_ID"                         AS "ExecutedBy",
-       "CustomerInvoice"."CreationExecutionType"                AS "ExecutionType"
-FROM "o_celonis_CustomerInvoice" AS "CustomerInvoice"
+SELECT 'CreateCustomerInvoice' || '_' || "CustomerInvoice"."ID"        AS "ID",
+       "CustomerInvoice"."ID"                                          AS "CustomerInvoice",
+       "CustomerInvoice"."CreationTime"                                AS "Time",
+       "CustomerInvoice"."CreatedBy_ID"                                AS "ExecutedBy",
+       "CustomerInvoice"."CreationExecutionType"                       AS "ExecutionType"
+       
+FROM "o_celonis_CustomerInvoice"        AS "CustomerInvoice"
 WHERE "CustomerInvoice"."CreationTime" IS NOT NULL
 
 =======================================================================================================================================================================
