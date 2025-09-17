f3f6b2cb4e090179995b428ab3478595cdaf9b80
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-17 09:49:45 +0530

    Update 36_CustomerAccountClearingAssignment.sql

--- 02_Internship/OCPM/01_Objects/36_CustomerAccountClearingAssignment.sql
+++ 02_Internship/OCPM/01_Objects/36_CustomerAccountClearingAssignment.sql
@@ -8,19 +8,20 @@ SELECT <%=sourceSystem%>  || COALESCE('CustomerAccountClearingAssignment_' || "B
                 || "BKPF_PAY"."BELNR"
                 || "BKPF_PAY"."GJAHR",
                 'CustomerAccountClearingAssignment_' || "ClearingPointer"."MANDT" || "ClearingPointer"."BUKRS"
-                || "ClearingPointer"."AUGBL" || "ClearingPointer"."AUGGJ")     AS "ID",
+                || "ClearingPointer"."AUGBL" || "ClearingPointer"."AUGGJ")     		AS "ID",
        CASE
            WHEN "BKPF_PAY"."MANDT" IS NOT NULL
                THEN TIMESTAMPADD(MILLISECOND, 1, CAST("BKPF_PAY"."CPUDT" AS DATE) + CAST(TIMESTAMPDIFF(SECOND, CAST("BKPF_PAY"."CPUTM" AS DATE),
                     "BKPF_PAY"."CPUTM") AS INTERVAL SECOND))
            ELSE CAST(CONCAT(CAST("ClearingPointer"."AUGDT" AS VARCHAR), '23:59:59') AS TIMESTAMP)
-           END                                                                 AS "CreationTime",
-	<%=sourceSystem%>  || 'User_' || "BKPF_PAY"."MANDT" || "BKPF_PAY"."USNAM"     AS "CreatedBy",
+           END                                                                 		AS "CreationTime",
+	<%=sourceSystem%>  || 'User_' || "BKPF_PAY"."MANDT" || "BKPF_PAY"."USNAM"     	AS "CreatedBy",
        CASE
            WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
-           ELSE 'Manual' END                                                   AS "CreationExecutionType",
-       'SAP'                                                                   AS "SourceSystemType",
-	<%=sourceSystem%>  || COALESCE("BKPF_PAY"."MANDT", "ClearingPointer"."MANDT") AS "SourceSystemInstance"
+           ELSE 'Manual' END                                                   		AS "CreationExecutionType",
+       'SAP'                                                                   		AS "SourceSystemType",
+	<%=sourceSystem%>  || COALESCE("BKPF_PAY"."MANDT", "ClearingPointer"."MANDT") 	AS "SourceSystemInstance"
+						
 FROM "CTE_ClearingPointer" AS "ClearingPointer"
          LEFT JOIN "BKPF" AS "BKPF_PAY"
                    ON "ClearingPointer"."MANDT" = "BKPF_PAY"."MANDT"
