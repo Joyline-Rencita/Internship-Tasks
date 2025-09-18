f56d60e1bb6af55b66612c53f1b6c67112cdeb6f
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-18 22:12:54 +0530

    Update and rename 43_CustomerInvoiceItem.sql to 43_Customer Invoice Item.sql

--- 02_Internship/OCPM/01_Objects/43_Customer Invoice Item.sql
+++ 02_Internship/OCPM/01_Objects/43_Customer Invoice Item.sql
@@ -1,13 +1,13 @@
               ***********************         VBRP      ****************************
 
-SELECT <%=sourceSystem%>  || 'CustomerInvoiceItem_' || "VBRP"."MANDT" || "VBRP"."VBELN" || "VBRP"."POSNR" AS "ID",
+SELECT <%=sourceSystem%>  || 'CustomerInvoiceItem_' || "VBRP"."MANDT" || "VBRP"."VBELN" || "VBRP"."POSNR" 	AS "ID",
     CAST("VBRP"."ERDAT" AS DATE) + CAST(TIMESTAMPDIFF(SECOND, CAST("VBRP"."ERZET" AS DATE),
-            "VBRP"."ERZET") AS INTERVAL SECOND)                                                           AS "CreationTime",
-	<%=sourceSystem%>  || 'CustomerInvoice_' || "VBRP"."MANDT" || "VBRP"."VBELN"                             AS "Header",
-	<%=sourceSystem%>  || 'User_' || "VBRP"."MANDT" || "VBRP"."ERNAM"                                        AS "CreatedBy",
+            "VBRP"."ERZET") AS INTERVAL SECOND)                                                           	AS "CreationTime",
+	<%=sourceSystem%>  || 'CustomerInvoice_' || "VBRP"."MANDT" || "VBRP"."VBELN"                            AS "Header",
+	<%=sourceSystem%>  || 'User_' || "VBRP"."MANDT" || "VBRP"."ERNAM"                                       AS "CreatedBy",
     CASE
         WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
-        ELSE 'Manual' END                                                                                 AS "CreationExecutionType",
+        ELSE 'Manual' END                                                                                 	AS "CreationExecutionType",
     'SAP'                                                                                                 AS "SourceSystemType",
 	<%=sourceSystem%>  || "VBRP"."MANDT"                                                                     AS "SourceSystemInstance",
     "VBRP"."VBELN"                                                                                        AS "SystemCustomerInvoiceNumber",
