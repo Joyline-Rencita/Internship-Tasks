86ce6843d4ad3eb48e545ec590ea8a78dfc41809
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-19 09:51:11 +0530

    Create 61_CreateDeliveryHeader.sql

---
+++ 02_Internship/OCPM/02_Events/61_CreateDeliveryHeader.sql
@@ -0,0 +1,9 @@
+                  *****************************        EVENT       ****************************
+
+SELECT 'CreateDeliveryHeader' || '_' || "Delivery"."ID" AS "ID",
+       "Delivery"."ID"                                  AS "Delivery",
+       "Delivery"."CreationTime"                        AS "Time",
+       "Delivery"."CreatedBy_ID"                        AS "ExecutedBy",
+       "Delivery"."CreationExecutionType"               AS "ExecutionType"
+FROM "o_celonis_Delivery" AS "Delivery"
+WHERE "Delivery"."CreationTime" IS NOT NULL
