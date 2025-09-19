33265b5cddc5a44b7bcb8f040ab1f7b140db8393
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-19 09:52:28 +0530

    Update 61_CreateDeliveryHeader.sql

--- 02_Internship/OCPM/02_Events/61_CreateDeliveryHeader.sql
+++ 02_Internship/OCPM/02_Events/61_CreateDeliveryHeader.sql
@@ -1,4 +1,5 @@
-                  *****************************        EVENT       ****************************
+                  *****************************        EVENT       **********************************
+                    
 
 SELECT 'CreateDeliveryHeader' || '_' || "Delivery"."ID" AS "ID",
        "Delivery"."ID"                                  AS "Delivery",
@@ -7,3 +8,26 @@ SELECT 'CreateDeliveryHeader' || '_' || "Delivery"."ID" AS "ID",
        "Delivery"."CreationExecutionType"               AS "ExecutionType"
 FROM "o_celonis_Delivery" AS "Delivery"
 WHERE "Delivery"."CreationTime" IS NOT NULL
+
+
+=======================================================================================================================================================================
+
+
+                                        RELATIONSHIPS ->  SALES ORDER ITEMS
+
+SELECT DISTINCT
+                "Event"."ID"  AS "ID",
+                "Object"."ID" AS "SalesOrderItems"
+FROM (SELECT "Event"."ID"                       AS "ID",
+             "DeliveryItem"."SalesOrderItem_ID" AS "SalesOrderItem_ID",
+             "Event"."Time"                     AS "Time"
+      FROM "e_celonis_CreateDeliveryHeader" AS "Event"
+               LEFT JOIN (SELECT "Header_ID"         AS "Header_ID",
+                                 "SalesOrderItem_ID" AS "SalesOrderItem_ID"
+                          FROM "o_celonis_DeliveryItem" AS "DeliveryItem"
+                          ORDER BY "DeliveryItem"."Header_ID") AS "DeliveryItem"
+                         ON "Event"."Delivery_ID" = "DeliveryItem"."Header_ID"
+      ORDER BY "DeliveryItem"."SalesOrderItem_ID") AS "Event"
+         LEFT JOIN "o_celonis_SalesOrderItem" AS "Object" ON "Event"."SalesOrderItem_ID" = "Object"."ID"
+      WHERE TIMESTAMPDIFF(SECOND, "Event"."Time", "Object"."CreationTime") <= 5
+        AND "Object"."ID" IS NOT NULL
