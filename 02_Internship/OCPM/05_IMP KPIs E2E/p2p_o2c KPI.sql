56b6f3849e9ef3e38ba15a0e0e415105f31dfc79
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-22 12:10:43 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -103,3 +103,20 @@ Credit Block Release Cycle Time :
 AVG(
   DAYS_BETWEEN ( "o_celonis_VendorAccountCreditItem"."CreationTime", "o_celonis_VendorAccountCreditItem"."ClearingDate" )
 )
+
+
+DPO (Days Payable Outstanding) :
+SUM(
+  CASE
+    WHEN "o_celonis_VendorAccountCreditItem"."isRelevantAndCleared"= 0.0
+    THEN 0.0
+    ELSE DAYS_BETWEEN(ROUND_DAY("o_celonis_VendorAccountCreditItem"."BaseLineDate"), ROUND_DAY("o_celonis_VendorAccountCreditItem"."ClearingDate"))
+      * "o_celonis_VendorAccountCreditItem"."ConvertedDocumentValue"
+  END)
+  /
+  SUM(
+    CASE
+      WHEN "o_celonis_VendorAccountCreditItem"."isRelevantAndCleared" = 0.0
+      THEN 0.0
+      ELSE "o_celonis_VendorAccountCreditItem"."ConvertedDocumentValue"
+    END)
