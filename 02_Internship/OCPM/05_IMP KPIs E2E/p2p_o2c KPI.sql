30ded462ff39b7ad7d3f08b25c58bd9960eaca47
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-22 12:09:11 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -99,3 +99,7 @@ COUNT(
 * 100
 
 
+Credit Block Release Cycle Time : 
+AVG(
+  DAYS_BETWEEN ( "o_celonis_VendorAccountCreditItem"."CreationTime", "o_celonis_VendorAccountCreditItem"."ClearingDate" )
+)
