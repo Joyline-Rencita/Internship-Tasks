a693c7059897791dd688b71e107f5862d5333b67
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-22 08:37:59 +0530

    Update 18_Create Vendor Credit Memo.sql

--- 02_Internship/OCPM/02_Events/18_Create Vendor Credit Memo.sql
+++ 02_Internship/OCPM/02_Events/18_Create Vendor Credit Memo.sql
@@ -30,5 +30,19 @@ WHERE "Object"."ID" IS NOT NULL
 
 =====================================================================================================================================================================
 
+                                RELATIONSHIPS -> PURCHASE ORDER ITEMS
 
+SELECT DISTINCT
+       "Event"."ID"  AS "ID",
+       "Object"."ID" AS "PurchaseOrderItems"
+FROM "e_celonis_CreateVendorCreditMemo" AS "Event"
+         LEFT JOIN "o_celonis_VendorCreditMemo" AS "VendorCreditMemo"
+                   ON "Event"."VendorCreditMemo_ID" = "VendorCreditMemo"."ID"
+         LEFT JOIN "o_celonis_VendorCreditMemoItem" AS "VendorCreditMemoItem"
+                   ON "VendorCreditMemo"."ID" = "VendorCreditMemoItem"."Header_ID"
+         LEFT JOIN "o_celonis_PurchaseOrderItem" AS "Object"
+                   ON "VendorCreditMemoItem"."PurchaseOrderItem_ID" = "Object"."ID"
+WHERE "Object"."ID" IS NOT NULL
+
+====================================================================================================================================================================
 
