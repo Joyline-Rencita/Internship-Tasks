dd0592b38cab5da4550cea15050c040b66a14593
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-23 19:16:33 +0530

    Update 51_Plant.sql

--- 02_Internship/OCPM/01_Objects/51_Plant.sql
+++ 02_Internship/OCPM/01_Objects/51_Plant.sql
@@ -6,7 +6,7 @@ SELECT <%=sourceSystem%>  || 'Plant_' || "T001W"."MANDT" || "T001W"."WERKS" AS "
     "T001W"."LAND1"                                                         AS "Country",
     "T005T"."LANDX"                                                         AS "CountryText",
     'SAP'                                                                   AS "SourceSystemType",
-	<%=sourceSystem%>  || "T001W"."MANDT"                                      AS "SourceSystemInstance"
+	<%=sourceSystem%>  || "T001W"."MANDT"                                   AS "SourceSystemInstance"
 FROM "T001W" AS "T001W"
          LEFT JOIN "T005T" AS "T005T"
                    ON "T001W"."MANDT" = "T005T"."MANDT"
