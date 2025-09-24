5321e4fc9bd1a5ce16f070cee5641b9122833bec
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-24 20:09:22 +0530

    Update 55_QuotationItem.sql

--- 02_Internship/OCPM/01_Objects/55_QuotationItem.sql
+++ 02_Internship/OCPM/01_Objects/55_QuotationItem.sql
@@ -1,12 +1,12 @@
               *****************************        VBAP        **********************************
 
 
-SELECT <%=sourceSystem%>  || 'QuotationItem_' || "VBAP"."MANDT" || "VBAP"."VBELN" || "VBAP"."POSNR" AS "ID",
+SELECT <%=sourceSystem%>  || 'QuotationItem_' || "VBAP"."MANDT" || "VBAP"."VBELN" || "VBAP"."POSNR" 	AS "ID",
     CAST("VBAP"."ERDAT" AS DATE)
             + CAST(TIMESTAMPDIFF(SECOND, CAST("VBAP"."ERZET" AS DATE),
-            "VBAP"."ERZET") AS INTERVAL SECOND)                                                     AS "CreationTime",
-	<%=sourceSystem%>  || 'Quotation_' || "VBAP"."MANDT" || "VBAP"."VBELN"                             AS "Header",
-	<%=sourceSystem%>  || 'User_' || "VBAP"."MANDT" || "VBAP"."ERNAM"                                  AS "CreatedBy",
+            "VBAP"."ERZET") AS INTERVAL SECOND)                                                     	AS "CreationTime",
+	<%=sourceSystem%>  || 'Quotation_' || "VBAP"."MANDT" || "VBAP"."VBELN"                             	AS "Header",
+	<%=sourceSystem%>  || 'User_' || "VBAP"."MANDT" || "VBAP"."ERNAM"                                  	AS "CreatedBy",
     CASE
         WHEN "USR02"."USTYP" IN ('B', 'C') THEN 'Automatic'
         ELSE 'Manual' END                                                                           AS "CreationExecutionType",
