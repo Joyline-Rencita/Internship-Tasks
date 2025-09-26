38e8048feda66853db860a23e896572ef297ae76
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-26 19:38:28 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -181,4 +181,22 @@ COUNT(
 END
 )
 
-13.  
+13.  Vendor OTIF :
+
+(
+  COUNT(
+    CASE
+      WHEN 
+        PU_FIRST("o_celonis_PurchaseOrderItem", "o_celonis_VendorConfirmation"."ConfirmationDeliveryDate") 
+          <= "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate"
+        AND "o_celonis_PurchaseOrderItem"."Quantity" = "o_celonis_PurchaseOrderScheduleLine"."ScheduledQuantity"
+      THEN "o_celonis_PurchaseOrderItem"."ID"
+    END
+  )
+/
+  COUNT(DISTINCT  "o_celonis_PurchaseOrderItem"."ID")
+) 
+* 100
+
+
+14.  
