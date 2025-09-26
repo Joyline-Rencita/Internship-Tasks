8fc27e54f9aaa3cb4833a08af6c1471e5aa976a5
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-26 19:33:35 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -152,3 +152,9 @@ AVG(
 
 10 :  PO creation to Delivery CT :
 
+AVG(
+  DAYS_BETWEEN(
+    BIND("o_celonis_RelationshipCustomerInvoiceItem", "o_celonis_Delivery"."DeliveryDate"),
+    PU_FIRST("o_celonis_MaterialMasterPlant", "o_celonis_PurchaseOrderItem"."CreationTime")
+  )
+)
