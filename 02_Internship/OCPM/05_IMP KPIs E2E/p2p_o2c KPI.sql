430830509be52865eb713295a4fb4855f616433b
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-26 19:59:08 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -36,9 +36,9 @@ AVG(
 (
   COUNT(
     CASE
-      WHEN "o_celonis_Delivery"."DeliveryDate" <= "o_celonis_SalesOrder"."RequestedDeliveryDate"                 -- On-time delivery check
-        AND "o_celonis_Delivery"."Customer_ID" = "o_celonis_SalesOrder"."Customer_ID"                            -- Customer match
-        AND "o_celonis_SalesOrderItem"."OrderedQuantity" = "o_celonis_DeliveryItem"."Quantity"                 -- Complete delivery check: Delivered quantity >= Ordered quantity
+      WHEN "o_celonis_Delivery"."DeliveryDate" <= "o_celonis_SalesOrder"."RequestedDeliveryDate"       -- On-time delivery check
+        AND "o_celonis_Delivery"."Customer_ID" = "o_celonis_SalesOrder"."Customer_ID"                  -- Customer match
+        AND "o_celonis_SalesOrderItem"."OrderedQuantity" = "o_celonis_DeliveryItem"."Quantity"         -- Complete delivery check: Delivered quantity >= Ordered quantity
       THEN "o_celonis_SalesOrder"."ID"
     END
   )
