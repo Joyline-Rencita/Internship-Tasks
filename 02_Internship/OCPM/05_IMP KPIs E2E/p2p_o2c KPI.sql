99b119eae01635929026f4ea3d0e4f676b0a8571
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-26 20:04:16 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -207,9 +207,7 @@ SUM(
    END
 )
 
-17.  
-
-
+17.    Supplier On-Time Delivery :
 
 
 
@@ -273,3 +271,40 @@ CASE
       SUM("o_celonis_VendorAccountCreditItem"."Amount")
     )
 END
+
+22. Orders Delayed Due to Supplier Issues :
+
+ROUND(
+  CASE
+    WHEN COUNT(
+          DISTINCT CASE WHEN "o_celonis_Delivery"."PickingDate" IS NOT NULL THEN "o_celonis_SalesOrder"."ID" END
+         ) = 0 THEN 0
+    ELSE 100 *
+      COUNT(
+        DISTINCT CASE
+          WHEN "o_celonis_Delivery"."DeliveryDate" > "o_celonis_SalesOrder"."RequestedDeliveryDate"
+               AND "o_celonis_Delivery"."PickingDate" > "o_celonis_SalesOrder"."RequestedDeliveryDate"
+          THEN "o_celonis_SalesOrder"."ID"
+        END
+      )
+      /
+      COUNT(
+        DISTINCT CASE WHEN "o_celonis_Delivery"."PickingDate" IS NOT NULL THEN "o_celonis_SalesOrder"."ID" END --not including the picking dates which are null
+      )
+  END,
+  2
+)
+
+
+23. 
+
+(
+  COUNT(
+    DISTINCT CASE
+      WHEN "o_celonis_Delivery"."DeliveryDate" > "o_celonis_SalesOrder"."RequestedDeliveryDate"
+      THEN "o_celonis_SalesOrder"."ID"
+    END
+  )
+  /
+  COUNT(DISTINCT "o_celonis_SalesOrder"."ID")
+)*100
