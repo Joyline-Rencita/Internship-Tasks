93e9b1b94ba1d4001c5bff43ab2d2a9ac18ddcdb
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-29 22:09:06 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -230,10 +230,19 @@ SUM(
 
 17.    Supplier On-Time Delivery :
 
+100 * (
+  COUNT (DISTINCT CASE
+    WHEN "o_celonis_IncomingMaterialDocumentItem"."CreationTime" >= ADD_DAYS(TODAY(), -7)
+    AND "o_celonis_IncomingMaterialDocumentItem"."ReversalFlag" = '0'
+    THEN "o_celonis_IncomingMaterialDocumentItem"."Id"
+  END)
+  /
+  COUNT (DISTINCT CASE
+    WHEN "o_celonis_IncomingMaterialDocumentItem"."ReversalFlag" = '0'
+    THEN "o_celonis_IncomingMaterialDocumentItem"."Id"
+  END)
+)
 
-
-
-  
 
 18.  Invoice Processing Time :     correction needed
 
