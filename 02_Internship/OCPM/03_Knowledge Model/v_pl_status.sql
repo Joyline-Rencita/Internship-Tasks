1008229033c278fcdaf69e16a7ffaeb1c2cf5b51
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-29 22:00:24 +0530

    Create v_pl_status.sql

---
+++ 02_Internship/OCPM/03_Knowledge Model/v_pl_status.sql
@@ -0,0 +1,10 @@
+v_pl_status :
+
+CASE WHEN "EKPO"."LOEKZ" IS NOT NULL THEN 'Deleted'
+WHEN "EKPO"."ELIKZ" IS NOT NULL THEN 'Delivery Completed'
+WHEN KPI("v_pl_gr_qty") >= KPI("v_pl_req_qty")  THEN 'Fully Received - PO Open'
+WHEN KPI("v_pl_gr_qty") <= 0 OR KPI("v_pl_gr_qty") is null THEN 
+    CASE WHEN "EKPO"."SES" IS NULL THEN 'NO GR' ELSE 'SES Available' END
+WHEN KPI("v_pl_gr_qty") < KPI("v_pl_req_qty") AND KPI("v_pl_gr_qty") > 0 THEN 'Partially Received'
+else 'Validate'
+end
