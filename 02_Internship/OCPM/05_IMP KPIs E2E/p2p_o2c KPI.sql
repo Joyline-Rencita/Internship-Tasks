0f766aa81aeadb0294abc5a2107d0e9761bf41c3
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-09-30 12:20:37 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -403,3 +403,17 @@ ROUND(
   /
   COUNT(DISTINCT "o_celonis_SalesOrder"."ID")
 )*100
+
+
+33. Delayed PO Creation :
+AVG(
+  CASE
+    WHEN DAYS_BETWEEN(
+      BIND("o_celonis_SalesOrderItem", "o_celonis_SalesOrder"."CreationTime"),
+      PU_FIRST("o_celonis_MaterialMasterPlant" ,BIND("o_celonis_PurchaseOrderItem", "o_celonis_PurchaseOrder"."CreationTime")
+      )
+    ) > 3
+    THEN 1
+    ELSE 0
+  END
+) * 100
