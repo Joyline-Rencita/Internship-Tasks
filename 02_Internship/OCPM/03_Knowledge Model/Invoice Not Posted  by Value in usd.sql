05b4810c10df6d438370925f8d2943029b111ca4
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-01 20:51:47 +0530

    Update Invoice Not Posted  by Value in usd.sql

--- 02_Internship/OCPM/03_Knowledge Model/Invoice Not Posted  by Value in usd.sql
+++ 02_Internship/OCPM/03_Knowledge Model/Invoice Not Posted  by Value in usd.sql
@@ -1,4 +1,6 @@
 SUM(
-  CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted' then
-  
-  "o_celonis_VendorAccountCreditItem"."Amount" END)
+  CASE 
+    WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted' 
+    THEN "o_celonis_VendorAccountCreditItem"."Amount" 
+  END
+)
