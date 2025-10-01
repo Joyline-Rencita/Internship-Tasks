f5c119be8c84a9b8d983ae50d4f9c18553ff3212
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-01 20:49:55 +0530

    Update Invoices Posted but not Cleared by Volume.sql

--- 02_Internship/OCPM/03_Knowledge Model/Invoices Posted but not Cleared by Volume.sql
+++ 02_Internship/OCPM/03_Knowledge Model/Invoices Posted but not Cleared by Volume.sql
@@ -1,4 +1,6 @@
 COUNT (
-  CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoices Posted but Not Cleared' then
-  
-  "o_celonis_VendorAccountCreditItem"."ID" END)
+  CASE 
+    WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoices Posted but Not Cleared' 
+    THEN "o_celonis_VendorAccountCreditItem"."ID" 
+  END
+)
