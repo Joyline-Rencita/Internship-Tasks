201c776bca6b3dcdc852c7997da23aa56d366b5f
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-01 19:43:03 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -458,3 +458,16 @@ SUM(
 )
 
 
+37.    Non Contract Suppliers:
+
+(
+  COUNT(
+    CASE
+      WHEN "o_celonis_PurchaseOrderItem"."ContractItem_ID" IS NULL
+      THEN "o_celonis_PurchaseOrderItem"."ID"
+    END
+  )
+/
+  COUNT(DISTINCT "o_celonis_PurchaseOrderItem"."ID")
+) * 100
+
