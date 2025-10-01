6398a9293d7ff00f20c492377cb94cf81dac7134
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-01 20:16:20 +0530

    Create Total Invoice Lines.sql

---
+++ 02_Internship/OCPM/03_Knowledge Model/Total Invoice Lines.sql
@@ -0,0 +1,1 @@
+COUNT("o_celonis_VendorAccountCreditItem"."CompanyCode"||"o_celonis_VendorAccountCreditItem"."SystemAccountingDocumentNumber"||"o_celonis_VendorAccountCreditItem"."SystemAccountingDocumentItemNumber"||"o_celonis_VendorAccountCreditItem"."FiscalYear")
