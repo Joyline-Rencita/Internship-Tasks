eb6c43485787d0c4f717b121df7b7ab8a55ddba8
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-01 20:50:37 +0530

    Update Invoices Posted but Not Cleared by Value(in USD).sql

--- 02_Internship/OCPM/03_Knowledge Model/Invoices Posted but Not Cleared by Value(in USD).sql
+++ 02_Internship/OCPM/03_Knowledge Model/Invoices Posted but Not Cleared by Value(in USD).sql
@@ -1,4 +1,5 @@
 SUM(
-  CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoices Posted but Not Cleared' then
-  
-  "o_celonis_VendorAccountCreditItem"."Amount" END)
+  CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoices Posted but Not Cleared' 
+  THEN "o_celonis_VendorAccountCreditItem"."Amount" 
+END
+)
