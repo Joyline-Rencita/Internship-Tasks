c5f9666d0d6adeda3276f86ae0cbc78e74eff76b
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-01 20:49:21 +0530

    Update Invoices not Posted by Volume.sql

--- 02_Internship/OCPM/03_Knowledge Model/Invoices not Posted by Volume.sql
+++ 02_Internship/OCPM/03_Knowledge Model/Invoices not Posted by Volume.sql
@@ -1,4 +1,6 @@
 COUNT(
-  CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted' then
-  
-  "o_celonis_VendorAccountCreditItem"."ID" END)
+  CASE 
+    WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted' 
+    THEN "o_celonis_VendorAccountCreditItem"."ID" 
+  END
+)
