c7edfff7380a0f1ef2fa395766ef445f9432761e
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-04 19:03:54 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -215,6 +215,14 @@ COUNT(
 
 count("o_celonis_SalesOrderItem"."ID")
 
+COUNT(
+  CASE
+    WHEN PU_FIRST("o_celonis_MaterialMasterPlant", "o_celonis_PurchaseOrderItem"."ID") IS NOT NULL
+    THEN "o_celonis_SalesOrderItem"."ID"
+    ELSE NULL
+  END
+)
+
 15 . SO linked with POs in Value :
 
 SUM ( "o_celonis_SalesOrderItem"."NetAmount" )
