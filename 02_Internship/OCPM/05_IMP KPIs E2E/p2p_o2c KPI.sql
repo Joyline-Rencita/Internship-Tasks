7fa5235b95b4e9d4be665df5366db4911a6a0dfb
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-04 12:47:59 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -512,3 +512,20 @@ SUM(
 /
   COUNT(DISTINCT "o_celonis_PurchaseOrderItem"."ID")
 ) * 100
+
+42. Vendor Late Delivery :
+
+(
+  COUNT(
+    CASE
+      WHEN 
+        PU_FIRST("o_celonis_PurchaseOrderItem", "o_celonis_VendorConfirmation"."ConfirmationDeliveryDate") 
+          > "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate"
+      THEN "o_celonis_PurchaseOrderItem"."ID"
+    END
+  )
+/
+  COUNT(DISTINCT "o_celonis_PurchaseOrderItem"."ID")
+) 
+* 100
+
