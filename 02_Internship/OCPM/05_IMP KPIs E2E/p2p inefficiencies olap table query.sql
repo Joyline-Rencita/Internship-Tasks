0487cde45856a9c52bf326767e78405fb197c6c7
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-04 12:56:40 +0530

    Update p2p inefficiencies olap table query.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p inefficiencies olap table query.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p inefficiencies olap table query.sql
@@ -104,4 +104,13 @@ END
 BIND("o_celonis_SalesOrderItem", "o_celonis_SalesOrder"."CreationTime")
 
 
-11. 
+11. PO Creation Time:
+
+PU_FIRST("o_celonis_MaterialMasterPlant" , 
+          BIND("o_celonis_PurchaseOrderItem", "o_celonis_PurchaseOrder"."CreationTime")
+
+)
+
+12. Goods Scheduled Qty :
+
+
