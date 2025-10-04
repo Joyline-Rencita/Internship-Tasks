da23cf3527268fa3b65ece05938045c56901eea5
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-04 19:04:24 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -227,6 +227,14 @@ COUNT(
 
 SUM ( "o_celonis_SalesOrderItem"."NetAmount" )
 
+SUM(
+  CASE
+    WHEN PU_FIRST("o_celonis_MaterialMasterPlant", "o_celonis_PurchaseOrderItem"."ID") IS NOT NULL
+    THEN "o_celonis_SalesOrderItem"."NetAmount"
+    ELSE NULL
+  END
+)
+
 16. PO Backlog Value :
 
 SUM(
