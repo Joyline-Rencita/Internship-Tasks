02276ebc741640c98d3bc561b10062cf1298da0d
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-05 10:43:46 +0530

    Update Invoice Not Posted Count.sql

--- 02_Internship/OCPM/03_Knowledge Model/Invoice Not Posted Count.sql
+++ 02_Internship/OCPM/03_Knowledge Model/Invoice Not Posted Count.sql
@@ -1,3 +1,5 @@
+Invoice Not Posted Count :
+
 COUNT(
    CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted'
         THEN "o_custom_OpenInvoiceDeepdiveManualFile"."ID"
