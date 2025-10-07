a962d2152a655676fe7846eef4cb049cf15664bc
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-07 22:18:01 +0530

    Update 40_PO Creation to Vendor Delivery CT.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/40_PO Creation to Vendor Delivery CT.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/40_PO Creation to Vendor Delivery CT.sql
@@ -1,8 +1,13 @@
 PO Creation to Vendor Delivery Cycle Time :
 
-AVG(
+AVG(        --- 8 DAYS
   DAYS_BETWEEN(
-    "o_celonis_PurchaseOrderItem"."CreationTime",
-    "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate"
+    PU_FIRST("o_celonis_MaterialMasterPlant" , 
+       BIND("o_celonis_PurchaseOrderItem", "o_celonis_PurchaseOrderItem"."CreationTime")
+    ), 
+    PU_FIRST("o_celonis_MaterialMasterPlant" , 
+      PU_FIRST("o_celonis_PurchaseOrderItem", "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate")
+    )
+    
   )
 )
