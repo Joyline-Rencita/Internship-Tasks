e8c2221f05a799d221313eaae659bf5e21e33a03
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-09 09:39:02 +0530

    Update p2p_o2c KPI.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/p2p_o2c KPI.sql
@@ -145,6 +145,15 @@ AVG(
 
 AVG(
   DAYS_BETWEEN(
+    "o_celonis_PurchaseOrderItem"."CreationTime",
+    "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate"
+    
+  )
+)  
+
+                    p2p connecting o2c. but not recommended
+AVG(
+  DAYS_BETWEEN(
     BIND("o_celonis_RelationshipCustomerInvoiceItem", "o_celonis_Delivery"."DeliveryDate"),
     PU_FIRST("o_celonis_MaterialMasterPlant", "o_celonis_PurchaseOrderItem"."CreationTime")
   )
