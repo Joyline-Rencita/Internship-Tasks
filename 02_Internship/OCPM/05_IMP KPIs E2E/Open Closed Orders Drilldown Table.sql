7a48680ca10ce308a85ebc900c375fa8883ea3fb
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-18 10:08:19 +0530

    Update Open Closed Orders Drilldown Table.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Open Closed Orders Drilldown Table.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/Open Closed Orders Drilldown Table.sql
@@ -4,3 +4,19 @@ Sales Order ID :
 SO Value :
 "o_celonis_SalesOrder"."NetAmount"
 
+Linked POs :
+COUNT(
+  CASE
+    WHEN PU_FIRST("o_celonis_MaterialMasterPlant", "o_celonis_PurchaseOrderItem"."ID") IS NOT NULL
+    THEN "o_celonis_SalesOrderItem"."ID"
+    ELSE NULL
+  END
+)
+
+Delivery Date(In-future/Today/passed) :
+CASE
+  WHEN "o_celonis_SalesOrder"."RequestedDeliveryDate" > TODAY() THEN 'Future'
+  WHEN "o_celonis_SalesOrder"."RequestedDeliveryDate" = TODAY() THEN 'Today'
+  WHEN "o_celonis_SalesOrder"."RequestedDeliveryDate" < TODAY() THEN 'Passed'
+  ELSE 'Unknown'
+END
