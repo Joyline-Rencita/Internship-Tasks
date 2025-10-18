a6bf04727b61004964ea26a9c6cb7a3cdd7ef332
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-18 10:13:04 +0530

    Update Open Closed Orders Drilldown Table.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Open Closed Orders Drilldown Table.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/Open Closed Orders Drilldown Table.sql
@@ -4,6 +4,9 @@ Sales Order ID :
 SO Value :
 "o_celonis_SalesOrder"."NetAmount"
 
+Purchase Order ID :
+PU_FIRST("o_celonis_MaterialMasterPlant",BIND("o_celonis_PurchaseOrderItem","o_celonis_PurchaseOrder"."ID"))
+
 Linked POs :
 COUNT(
   CASE
@@ -53,3 +56,58 @@ CASE
   ELSE 'Not at Risk'
 END
 
+
+
+
+================================================================================================================================================================
+
+                                                  FILTERS USED
+
+FILTER (PU_FIRST ("o_celonis_MaterialMasterPlant", "o_celonis_PurchaseOrderItem"."ID") is not null);
+FILTER ISNULL(PU_FIRST("o_celonis_SalesOrderItem",BIND("o_celonis_RelationshipCustomerInvoiceItem",BIND("o_celonis_DeliveryItem","o_celonis_Delivery"."DeliveryDate")))) = 1;
+FILTER ISNULL(CASE
+
+ WHEN 
+     PU_FIRST("o_celonis_MaterialMasterPlant" , 
+            PU_FIRST("o_celonis_PurchaseOrderItem",  "o_celonis_VendorConfirmation"."ConfirmationDeliveryDate"  )
+        )     
+          <
+          
+      PU_FIRST("o_celonis_MaterialMasterPlant" , 
+            PU_FIRST("o_celonis_PurchaseOrderItem",  "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate"  )
+        )
+          
+  THEN '   Vendor Late Delivery'
+
+  /*WHEN PU_COUNT("o_celonis_MaterialMasterPlant" ,
+        BIND ("o_celonis_PurchaseOrderItem",
+        PU_COUNT("o_celonis_Vendor", "o_celonis_PurchaseOrder"."ID"))) = 1
+  THEN 'One Time Vendors'
+
+   
+
+  WHEN DAYS_BETWEEN(
+      BIND("o_celonis_SalesOrderItem", "o_celonis_SalesOrder"."CreationTime"),
+      PU_FIRST("o_celonis_MaterialMasterPlant" ,BIND("o_celonis_PurchaseOrderItem", "o_celonis_PurchaseOrder"."CreationTime")
+      )
+    ) > 3
+    THEN 'Delayed PO Creation'
+
+   
+
+  WHEN PU_FIRST("o_celonis_MaterialMasterPlant" , 
+            PU_FIRST("o_celonis_PurchaseOrderItem", "o_celonis_PurchaseOrderScheduleLine"."GoodsReceivedQuantity")
+        )  < 
+        PU_FIRST("o_celonis_MaterialMasterPlant" , 
+            PU_FIRST("o_celonis_PurchaseOrderItem", "o_celonis_PurchaseOrderScheduleLine"."ScheduledQuantity")
+        )
+  THEN 'Vendor not in Full Delivery'
+
+  WHEN PU_FIRST("o_celonis_MaterialMasterPlant" , 
+          BIND("o_celonis_PurchaseOrderItem","o_celonis_PurchaseOrderItem"."ContractItem_ID")  
+      ) IS NULL
+  THEN 'Non Contract Suppliers'
+
+   */
+
+END) = 0
