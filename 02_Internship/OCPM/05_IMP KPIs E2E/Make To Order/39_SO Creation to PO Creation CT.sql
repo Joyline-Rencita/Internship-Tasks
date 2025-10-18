28d10f512825b0a6928a684ebf964b8a1119d6a5
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-18 09:55:43 +0530

    Update 39_SO Creation to PO Creation CT.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/39_SO Creation to PO Creation CT.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/39_SO Creation to PO Creation CT.sql
@@ -7,3 +7,12 @@ AVG(
     
   )
 )
+
+
+AVG(
+  DATEDIFF(DD,
+    PU_FIRST( "o_celonis_MaterialMasterPlant",BIND("o_celonis_SalesOrderItem","o_celonis_SalesOrder"."CreationTime")),
+    PU_FIRST("o_celonis_MaterialMasterPlant",BIND("o_celonis_PurchaseOrderItem","o_celonis_PurchaseOrder"."CreationTime"))
+    
+  )
+)
