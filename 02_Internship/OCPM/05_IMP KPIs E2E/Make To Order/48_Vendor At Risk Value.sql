71b55926a633a8bb4983ad127f7f95a9d2773a59
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-18 09:54:45 +0530

    Create 48_Vendor At Risk Value.sql

---
+++ 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/48_Vendor At Risk Value.sql
@@ -0,0 +1,25 @@
+Vendor At Risk Value :
+
+SUM(
+  CASE
+    WHEN
+      PU_FIRST(
+        "o_celonis_MaterialMasterPlant",
+        BIND(
+          "o_celonis_SalesOrderItem",
+          "o_celonis_SalesOrder"."RequestedDeliveryDate"
+        ),
+        ORDER BY "o_celonis_SalesOrder"."RequestedDeliveryDate" ASC
+      ) <= ADD_DAYS(TODAY(),7)
+      AND
+      PU_FIRST(
+        "o_celonis_MaterialMasterPlant",
+        PU_FIRST(
+          "o_celonis_PurchaseOrderItem",
+          "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate",
+          ORDER BY "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate" ASC
+        )
+      ) < TODAY()
+    THEN "o_celonis_SalesOrder"."NetAmount"
+  END
+)
