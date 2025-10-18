a5ca6bc063ba59ce4036b4625480a4b5d2861131
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-18 09:53:38 +0530

    Update 22_SO linked with POs in Value.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/22_SO linked with POs in Value.sql
+++ 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/22_SO linked with POs in Value.sql
@@ -9,3 +9,13 @@ SUM(
     ELSE NULL
   END
 )
+
+
+SUM(
+  CASE
+    WHEN PU_FIRST("o_celonis_MaterialMasterPlant",BIND( "o_celonis_PurchaseOrderItem","o_celonis_PurchaseOrder"."ID")) IS NOT NULL
+    THEN "o_celonis_SalesOrderItem"."NetAmount"
+    ELSE NULL
+  END
+)
+
