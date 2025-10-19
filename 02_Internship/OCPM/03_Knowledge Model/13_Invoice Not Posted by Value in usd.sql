a608b297951a3ae9a43eb08b52021cc2e56ee384
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-19 16:10:17 +0530

    Delete 02_Internship/OCPM/03_Knowledge Model/13_Invoice Not Posted by Value in usd.sql

--- 02_Internship/OCPM/03_Knowledge Model/13_Invoice Not Posted by Value in usd.sql
+++
@@ -1,8 +0,0 @@
-Invoice Not Posted by Value in USD :
-
-SUM(
-  CASE 
-    WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted' 
-    THEN "o_celonis_VendorAccountCreditItem"."Amount" 
-  END
-)
