bfc9e45784980529f3e190a887163167ca07e85c
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-19 16:02:27 +0530

    Delete 02_Internship/OCPM/03_Knowledge Model/03_past_due_days_v.sql

--- 02_Internship/OCPM/03_Knowledge Model/03_past_due_days_v.sql
+++
@@ -1,6 +0,0 @@
-Past_due_days_v :
-  
-CASE WHEN ADD_DAYS ( "o_celonis_VendorAccountCreditItem"."BaseLineDate",
-KPI("maximun_available_payment_days"))< TODAY() 
-THEN DAYS_BETWEEN ( ADD_DAYS ( "o_celonis_VendorAccountCreditItem"."BaseLineDate",
-KPI("maximun_available_payment_days")), TODAY()) END
