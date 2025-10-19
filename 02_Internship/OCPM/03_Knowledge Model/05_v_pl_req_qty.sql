f1b3af84b35f49eaf5b7e294808002cb960292b7
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-19 16:03:29 +0530

    Update 05_v_pl_req_qty.sql

--- 02_Internship/OCPM/03_Knowledge Model/05_v_pl_req_qty.sql
+++ 02_Internship/OCPM/03_Knowledge Model/05_v_pl_req_qty.sql
@@ -1,2 +1,3 @@
 v_pl_req_qty :
-"o_celonis_PurchaseOrderItem"."Quantity" * ((100 - TO_INT("o_celonis_PurchaseOrderItem"."ToleranceLimit"))/100)
+  
+"o_celonis_PurchaseOrderItem"."Quantity" * ((100 - TO_INT("o_celonis_PurchaseOrderItem"."ToleranceLimit")) /100 )
