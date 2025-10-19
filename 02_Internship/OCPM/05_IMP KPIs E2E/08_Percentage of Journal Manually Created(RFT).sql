b867c84a2971c7636cd8faaa8f0ac693be140532
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-19 16:27:46 +0530

    Delete 02_Internship/OCPM/05_IMP KPIs E2E/08_Percentage of Journal Manually Created(RFT).sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/08_Percentage of Journal Manually Created(RFT).sql
+++
@@ -1,7 +0,0 @@
-COUNT(CASE WHEN "o_custom_JournalReport"."Status"='OK' THEN "o_custom_JournalReport"."ID" ELSE NULL END)
-/ 
-(COUNT(CASE WHEN "o_custom_JournalReport"."Status"='OK' THEN "o_custom_JournalReport"."ID" ELSE NULL END) 
-+ 
-COUNT(DISTINCT CASE WHEN "o_custom_JournalReport"."Status"='RV' THEN "o_custom_JournalReport"."ID" ELSE NULL END) 
-+ 
-COUNT(DISTINCT CASE WHEN "o_custom_JournalReport"."Status"='RE' THEN "o_custom_JournalReport"."ID" ELSE NULL END))
