bfa0b954213cfa5cb4e1083c3f6363ceae5519c0
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-19 16:09:51 +0530

    Delete 02_Internship/OCPM/03_Knowledge Model/12_Invoice Not Posted Count.sql

--- 02_Internship/OCPM/03_Knowledge Model/12_Invoice Not Posted Count.sql
+++
@@ -1,7 +0,0 @@
-Invoice Not Posted Count :
-
-COUNT(
-   CASE WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted'
-        THEN "o_custom_OpenInvoiceDeepdiveManualFile"."ID"
-   END
-)
