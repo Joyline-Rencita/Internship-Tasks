91cb9ab61ec05e4929366bfb722b62d0a37b623d
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-20 10:08:52 +0530

    Delete 02_Internship/OCPM/01_Objects/06_Invoice Life cycle StageSheet1 - 1.sql

--- 02_Internship/OCPM/01_Objects/06_Invoice Life cycle StageSheet1 - 1.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    CAST(ROW_NUMBER() OVER (ORDER BY "INVOICE LIFECYCLE STAGE") AS VARCHAR(255))        AS "ID",
-    "INVOICE LIFECYCLE STAGE"                                                           AS "InvoiceLifecycleStage",
-    "# OF INVOICES"                                                                     AS "OfInvoices",
-    "INVOICE VALUE (IN MILLIONS)"                                                       AS "InvoiceValueinMillions"
-    
-FROM "Invoice_Lifecycle_Stage_Sheet1"
