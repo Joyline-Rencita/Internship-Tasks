2ca5bcb5ecc1f5b7508a5a86e4a4e656251a35a2
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-20 10:08:05 +0530

    Delete 02_Internship/OCPM/01_Objects/04_Exception Type Sheet1.sql

--- 02_Internship/OCPM/01_Objects/04_Exception Type Sheet1.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    CAST(ROW_NUMBER() OVER (ORDER BY "EXCEPTION TYPE") AS VARCHAR(255)) AS "ID",
-    "EXCEPTION TYPE"          AS "ExceptionType",
-    "# OF INVOICES"           AS "OfInvoices",
-    "_ EXCEPTION"             AS "Exception"
-    
-FROM "Exception_Type_Sheet1"
