e633c6cda84a13c597548fa8c97511f2f4f01a60
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-23 17:50:26 +0530

    Delete 02_Internship/OCPM/02_Events/58_CreateSalesOrderHeader.sql

--- 02_Internship/OCPM/02_Events/58_CreateSalesOrderHeader.sql
+++
@@ -1,9 +0,0 @@
-          *****************************       EVENT      ****************************
-
-SELECT 'CreateSalesOrderHeader' || '_' || "SalesOrder"."ID" AS "ID",
-       "SalesOrder"."ID"                                    AS "SalesOrder",
-       "SalesOrder"."CreationTime"                          AS "Time",
-       "SalesOrder"."CreatedBy_ID"                          AS "ExecutedBy",
-       "SalesOrder"."CreationExecutionType"                 AS "ExecutionType"
-FROM "o_celonis_SalesOrder" AS "SalesOrder"
-WHERE "SalesOrder"."CreationTime" IS NOT NULL
