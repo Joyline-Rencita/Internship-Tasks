5cb94be94766e7cf97e1f376b9933325da2d03c1
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-23 17:49:52 +0530

    Delete 02_Internship/OCPM/02_Events/56_CreateQuotationHeader.sql

--- 02_Internship/OCPM/02_Events/56_CreateQuotationHeader.sql
+++
@@ -1,9 +0,0 @@
-                        ***********************           EVENT              ***************************
-
-SELECT 'CreateQuotationHeader' || '_' || "Quotation"."ID" AS "ID",
-       "Quotation"."ID"                                   AS "Quotation",
-       "Quotation"."CreationTime"                         AS "Time",
-       "Quotation"."CreatedBy_ID"                         AS "ExecutedBy",
-       "Quotation"."CreationExecutionType"                AS "ExecutionType"
-FROM "o_celonis_Quotation" AS "Quotation"
-WHERE "Quotation"."CreationTime" IS NOT NULL
