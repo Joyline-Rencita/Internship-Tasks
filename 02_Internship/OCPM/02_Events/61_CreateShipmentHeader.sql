d65ce24f760e61e514909e1d812b651be872b67b
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-23 17:51:08 +0530

    Delete 02_Internship/OCPM/02_Events/61_CreateShipmentHeader.sql

--- 02_Internship/OCPM/02_Events/61_CreateShipmentHeader.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'CreateShipmentHeader' || '_' || "Shipment"."ID" AS "ID",
-       "Shipment"."ID"                                  AS "Shipment",
-       "Shipment"."CreationTime"                        AS "Time",
-       "Shipment"."CreatedBy_ID"                        AS "ExecutedBy",
-       "Shipment"."CreationExecutionType"               AS "ExecutionType"
-FROM "o_celonis_Shipment" AS "Shipment"
-WHERE "Shipment"."CreationTime" IS NOT NULL
