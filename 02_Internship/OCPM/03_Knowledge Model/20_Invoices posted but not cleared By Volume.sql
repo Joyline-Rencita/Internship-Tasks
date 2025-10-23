97b38a47b6cca09eecb09790ebe6c7fccfa240d6
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-23 18:08:17 +0530

    Delete 02_Internship/OCPM/03_Knowledge Model/20_Invoices posted but not cleared By Volume.sql

--- 02_Internship/OCPM/03_Knowledge Model/20_Invoices posted but not cleared By Volume.sql
+++
@@ -1,9 +0,0 @@
-Invoices posted but not cleared -By Volume :
-
-COUNT( 
-  DISTINCT CASE 
-    WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."Status" = 'Invoices Posted but Not Cleared' 
-            AND "o_celonis_VendorAccountCreditItem"."ClearingDate" iS NULL 
-    THEN "o_custom_OpenInvoiceDeepdiveManualFile"."BELNR"||"o_custom_OpenInvoiceDeepdiveManualFile"."BUKRS"||"o_custom_OpenInvoiceDeepdiveManualFile"."GJAHR" 
-  END
-)
