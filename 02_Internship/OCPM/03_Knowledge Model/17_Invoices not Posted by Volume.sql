9a23340d58838b4a8014788410170a29bcb278d8
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-23 18:07:09 +0530

    Delete 02_Internship/OCPM/03_Knowledge Model/17_Invoices not Posted by Volume.sql

--- 02_Internship/OCPM/03_Knowledge Model/17_Invoices not Posted by Volume.sql
+++
@@ -1,8 +0,0 @@
-Invoices not Posted by Volume :
-
-COUNT(
-  CASE 
-    WHEN "o_custom_OpenInvoiceDeepdiveManualFile"."STATUS" = 'Invoice Not Posted' 
-    THEN "o_celonis_VendorAccountCreditItem"."ID" 
-  END
-)
