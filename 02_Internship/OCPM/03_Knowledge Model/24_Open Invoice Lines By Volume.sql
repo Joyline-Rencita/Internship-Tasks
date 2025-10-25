2a5447fa8de898883cec081398e32df41138ff19
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-25 10:39:10 +0530

    Delete 02_Internship/OCPM/03_Knowledge Model/24_Open Invoice Lines By Volume.sql

--- 02_Internship/OCPM/03_Knowledge Model/24_Open Invoice Lines By Volume.sql
+++
@@ -1,7 +0,0 @@
-Open Invoice Lines By Volume :
-  
-COUNT(
-   CASE WHEN "o_celonis_VendorAccountCreditItem"."ClearingDate" IS NULL
-        THEN "o_custom_OpenInvoiceDeepdiveManualFile"."ID"
-   END
-)
