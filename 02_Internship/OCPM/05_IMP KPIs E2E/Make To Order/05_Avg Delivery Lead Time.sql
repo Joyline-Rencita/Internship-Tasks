ca2d1ab4dcab8fa523c4428a1c06d724f4611d00
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-25 11:32:33 +0530

    Delete 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/05_Avg Delivery Lead Time.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/05_Avg Delivery Lead Time.sql
+++
@@ -1,8 +0,0 @@
-Avg Delivery Lead Time :  Average number of days between order creation and delivery to customer.
-
-AVG(
-  DAYS_BETWEEN(
-    "o_celonis_Delivery"."ExpectedGoodsIssueDate",
-    "o_celonis_SalesOrder"."CreationTime"
-  )
-) * 100
