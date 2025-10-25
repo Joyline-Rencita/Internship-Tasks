0dc291c650877cc1cea6784aec0b0f8f651606fa
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-25 11:33:30 +0530

    Delete 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/09_PO Backlog Value.sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/09_PO Backlog Value.sql
+++
@@ -1,9 +0,0 @@
-PO Backlog Value :
-
-SUM(
-  CASE 
-    WHEN "o_celonis_PurchaseOrderScheduleLine"."ItemDeliveryDate" is NULL
-      AND "o_celonis_PurchaseOrderScheduleLine"."PurchaseOrderItem_ID" = "o_celonis_PurchaseOrderItem"."ID"
-    THEN ( "o_celonis_PurchaseOrderItem"."NetAmount")
-   END
-)
