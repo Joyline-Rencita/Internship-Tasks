02dd4f28b10d4394e6cbbc85b3adca75fe73ffa8
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-25 10:39:55 +0530

    Delete 02_Internship/OCPM/03_Knowledge Model/27_Total Invoice Lines.sql

--- 02_Internship/OCPM/03_Knowledge Model/27_Total Invoice Lines.sql
+++
@@ -1,5 +0,0 @@
-Total Invoice Lines :
-
-COUNT("o_celonis_VendorAccountCreditItem"."CompanyCode"||"o_celonis_VendorAccountCreditItem"."SystemAccountingDocumentNumber"
-  ||"o_celonis_VendorAccountCreditItem"."SystemAccountingDocumentItemNumber"||"o_celonis_VendorAccountCreditItem"."FiscalYear"
-  )
