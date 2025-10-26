b96dc9c9184aabac52a833f6eafffd78c5107053
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-10-26 09:33:18 +0530

    Delete 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/37_Delivery Date(In-future_Today_passed).sql

--- 02_Internship/OCPM/05_IMP KPIs E2E/Make To Order/37_Delivery Date(In-future_Today_passed).sql
+++
@@ -1,6 +0,0 @@
-CASE
-  WHEN "o_celonis_SalesOrder"."RequestedDeliveryDate" > TODAY() THEN 'Future'
-  WHEN "o_celonis_SalesOrder"."RequestedDeliveryDate" = TODAY() THEN 'Today'
-  WHEN "o_celonis_SalesOrder"."RequestedDeliveryDate" < TODAY() THEN 'Passed'
-  ELSE 'Unknown'
-END
