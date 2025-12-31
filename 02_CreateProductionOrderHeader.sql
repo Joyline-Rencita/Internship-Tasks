59188422a43674273412e2269f3440191e9f789a
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-12-31 20:03:34 +0530

    Delete 02_CreateProductionOrderHeader.sql

--- 02_CreateProductionOrderHeader.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'CreateProductionOrderHeader' || '_' || "ProductionOrder"."ID" AS "ID",
-       "ProductionOrder"."ID"                                         AS "ProductionOrder",
-       "ProductionOrder"."CreationTime"                               AS "Time",
-       "ProductionOrder"."CreatedBy_ID"                               AS "ExecutedBy",
-       "ProductionOrder"."CreationExecutionType"                      AS "ExecutionType"
-FROM "o_celonis_ProductionOrder" AS "ProductionOrder"
-WHERE "ProductionOrder"."CreationTime" IS NOT NULL
