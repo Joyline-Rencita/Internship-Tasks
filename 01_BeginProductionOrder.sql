ec22436f4328ad1eeb6ac5db5d18424128d3af0c
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-12-31 20:03:20 +0530

    Delete 01_BeginProductionOrder.sql

--- 01_BeginProductionOrder.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'BeginProductionOrder' || '_' || "ProductionOrder"."ID" AS "ID",
-       "ProductionOrder"."ID"                                  AS "ProductionOrder",
-       "ProductionOrder"."StartTime"                           AS "Time",
-       NULL                                                    AS "ExecutedBy",
-       NULL                                                    AS "ExecutionType"
-FROM "o_celonis_ProductionOrder" AS "ProductionOrder"
-WHERE "ProductionOrder"."StartTime" IS NOT NULL
