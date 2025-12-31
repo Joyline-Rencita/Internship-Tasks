24cac5b8692aa998e84e374ed92257838bf1092f
Joyline Rencita D'souza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-12-31 20:03:47 +0530

    Delete 03_FinishProductionOrder.sql

--- 03_FinishProductionOrder.sql
+++
@@ -1,7 +0,0 @@
-SELECT 'FinishProductionOrder' || '_' || "ProductionOrder"."ID" AS "ID",
-       "ProductionOrder"."ID"                                   AS "ProductionOrder",
-       "ProductionOrder"."FinishTime"                           AS "Time",
-       NULL                                                     AS "ExecutedBy",
-       NULL                                                     AS "ExecutionType"
-FROM "o_celonis_ProductionOrder" AS "ProductionOrder"
-WHERE "ProductionOrder"."FinishTime" IS NOT NULL
